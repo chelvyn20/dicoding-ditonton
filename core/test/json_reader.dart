193a474170bcdb08c4ba26855bbb792f98733e3f
chelvyn20 <chelvyn20@gmail.com>
2021-12-04 09:03:58 +0700

    commit -m "json"

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -3,8 +3,7 @@ import 'dart:io';
 String readJson(String name) {
   var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
-    // dir = dir.replaceAll('/test', '');
-    return File('$dir/$name').readAsStringSync();
+    dir = dir.replaceAll('/test', '');
   }
   if (dir.endsWith('core')) {
   return File('$dir/test/$name').readAsStringSync();
